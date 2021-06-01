bbbca0849422e4b8d2373b5c814ab69860b2cd2b
DeveloperAlexRamirez <alexkey0516@gmail.com>
2021-06-01 11:31:03 -0500

    Second commit

--- src/com/alex/Metodos.java
+++ src/com/alex/Metodos.java
@@ -10,10 +10,8 @@ public class Metodos {
     Scanner re = new Scanner(System.in);
     DecimalFormat dec = new DecimalFormat("#.000");
 
-    // Valores integral simple
     float n, a, b, C, D, h, A;
 
-    // Valores integral doble
     float m, c, d, E, F, G, h2;
 
     float resultado;
@@ -36,7 +34,6 @@ public class Metodos {
 
         System.out.println("Ingrese el valor de [D] ");
         this.D = re.nextFloat();
-        // valor de h
         this.h = (b - a) / n;
         this.A = a;
 
@@ -59,7 +56,6 @@ public class Metodos {
         System.out.println("Ingrese el valor de [G] ");
         G = re.nextFloat();
 
-        // valor de h
         this.h = (D - C) / m;
         this.c = C;
 
@@ -72,51 +68,35 @@ public class Metodos {
         r = r + 1;
         System.out.println("Valor actual de r: " + r);
 
-        System.out.println("a: " + this.a);
-        System.out.println("b: " + this.b);
-        System.out.println("C: " + this.C);
-        System.out.println("D: " + this.D);
-
         float dim[] = new float[r];
 
         for (int i = 1; i < (r - 1); i++) {
-            // Aquí vamos acumulando esa sumatoria (límite inferior + h)
-            a += h;
-            // Y la guardamos en el arreglo
+            a = a + h;
             dim[i] = a;
         }
 
-        // le damos valores a x0 y xn
-        dim[0] = A; // 0
+        dim[0] = A;
         dim[r - 1] = b;
         System.out.println("\n");
 
         for (int i = 0; i < r; i++) {
 
-            // System.out.println("x");
-            // System.out.println("|------------------|");
             System.out.println("El valor de x" + i + "=" + dec.format(dim[i]));
 
         }
-        // se realiza el remplazo de los valores en la funcion
         float fi, fa, fb, sum, multiplica;
         sum = 0;
 
         for (int i = 0; i < r; i++) {
-            // C = 1 (en el problema)
-            // arreglo con todos los valores de x
+
             fi = (float) sqrt(C * dim[i] + D);
-            // fi es el resultado de la raiz 1, 1.15, 1.29, 1.41 ...
-            // o sea: f1(x)...etc
+
             dim[i] = fi;
         }
 
-        // Aquí asignamos a fa y fb sus respectivos valores
-        // Esto, debido a que son la primera y última posición
         fa = dim[0];
         fb = dim[r - 1];
 
-        // comprobacion
         for (int i = 0; i < r; i++) {
             System.out.println("El valor de f" + i + "(x)= " + dec.format(dim[i]));
         }
@@ -126,9 +106,6 @@ public class Metodos {
             sum = sum + multiplica;
         }
         System.out.println("\n");
-
-        // System.out.println("resultado de prueba : " + (fa + fb));
-        // r final
 
         this.resultado = ((h / 2) * (sum + fa + fb));
         System.out.println("Resultado:" + this.resultado);
@@ -165,7 +142,6 @@ public class Metodos {
         System.out.println("Ingrese el valor de [G] ");
         G = re.nextFloat();
 
-        // valor de h
         h = (b - a) / n;
         A = a;
         h2 = (D - C) / m;
@@ -174,19 +150,17 @@ public class Metodos {
         System.out.println("DY");
 
         System.out.println("El valor de h es:" + h);
-        // cambio de n a r
 
         int r = (int) n;
         r = r + 1;
 
         float dim[] = new float[r];
 
-        // al limite inferior le sumamos h desde x1 hasta x(n-1)
         for (int i = 1; i < (r - 1); i++) {
             a = a + h;
             dim[i] = a;
         }
-        // le damos valores a x0 y xn
+
         dim[0] = A;
         dim[r - 1] = b;
         System.out.println("\n");
@@ -195,7 +169,6 @@ public class Metodos {
             System.out.println("El valor de x" + i + "=" + dec.format(dim[i]));
 
         }
-        // se realiza el remplazo de los valores en la funcion
         float fi, fa, fb, sum, resultado, multiplica;
         sum = 0;
 
@@ -205,7 +178,6 @@ public class Metodos {
         }
         fa = dim[0];
         fb = dim[r - 1];
-        // comprobacion
         for (int i = 0; i < r; i++) {
             System.out.println("El valor de f" + i + "(x)=" + dec.format(dim[i]));
         }
@@ -216,27 +188,22 @@ public class Metodos {
         }
         System.out.println("\n");
 
-        // r final
-
         resultado = ((h / 2) * (sum + fa + fb));
         System.out.println("Resultado:" + resultado);
 
         System.out.println("DX");
 
         System.out.println("El valor de h es:" + h2);
-        // cambio de n a r
 
         int r2 = (int) m;
         r2 = r2 + 1;
 
         float dim2[] = new float[r2];
 
-        // al limite inferior le sumamos h desde x1 hasta x(n-1)
         for (int u = 1; u < (r2 - 1); u++) {
             C = C + h2;
             dim2[u] = C;
         }
-        // le damos valores a x0 y xn
         dim2[0] = c;
         dim2[r2 - 1] = D;
         System.out.println("\n");
@@ -245,7 +212,7 @@ public class Metodos {
             System.out.println("El valor de x" + u + "=" + dec.format(dim2[u]));
 
         }
-        // se realiza el remplazo de los valores en la funcion
+
         float fi2, fa2, fb2, sum2, resultado2, multiplica2;
         sum2 = 0;
 
@@ -255,7 +222,7 @@ public class Metodos {
         }
         fa2 = dim2[0];
         fb2 = dim2[r2 - 1];
-        // comprobacion
+
         for (int u = 0; u < r2; u++) {
             System.out.println("El valor de f" + u + "(x)=" + dec.format(dim2[u]));
         }
@@ -265,8 +232,6 @@ public class Metodos {
             sum2 = sum2 + multiplica2;
         }
         System.out.println("\n");
-
-        // r final
 
         resultado2 = ((h2 / 2) * (sum2 + fa2 + fb2));
         System.out.println("Resultado:" + resultado2);
